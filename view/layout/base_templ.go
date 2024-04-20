2f61996e48d6e299bf169557e55e573e9ae89e32
0ussamabernou <oussama.bernou@outlook.com>
2024-04-20 22:09:10 +0100

    update

--- view/layout/base_templ.go
+++ view/layout/base_templ.go
@@ -36,7 +36,7 @@ func Base(title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title></head><body>this is from base layout")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</title></head><body>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
